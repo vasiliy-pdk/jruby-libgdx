c0239f43f5a330f33a1f6ff462ca6daa7a4298a0
Vasyl Pedak <truvazia@gmail.com>
2020-02-13 23:16:54 +0000

    Increase resolution

--- bin/jar-bootstrap.rb
+++ bin/jar-bootstrap.rb
@@ -2,9 +2,9 @@ require_relative '../lib/libgdx.rb'
 require_relative '../lib/game.rb'
 
 config = LwjglApplicationConfiguration.new()
-config.title = 'JRuby libGDX Hello World Example'
-config.width = 800
-config.height = 600
+config.title = 'JRuby libGDX 3D Sampe'
+config.width = 1900
+config.height = 1080
 config.resizable = true
 
 LwjglApplication.new(Game.new, config)
