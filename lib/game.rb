edd0863e8f96b83df6e8009027c6ae7c3211b7fa
Vasyl Pedak <truvazia@gmail.com>
2020-02-13 23:23:01 +0000

    Clean up the code slightly

--- lib/game.rb
+++ lib/game.rb
@@ -24,20 +24,20 @@ class Game
     @camera_controller = init_camera_controller
 
     @model_builder = ModelBuilder.new
-    @model = model_builder.createBox(5.0, 5.0, 5.0, 
+    @model = model_builder.createBox(
+      5.0, 5.0, 5.0, 
       Material.new(ColorAttribute.create_diffuse(ColorBuilder.from_int_rgb(224, 17, 95))),
       VertexAttributes::Usage::Position | VertexAttributes::Usage::Normal
     )
-    @model_instance = ModelInstance.new @model
+    @model_instance = ModelInstance.new model
 
     @model_batch = ModelBatch.new
   end
 
   def render
-    Gdx.gl.gl_viewport(0, 0, Gdx.graphics.width(), Gdx.graphics.height())
-    r, g, b = ColorBuilder.rgb_from_hex '032D1D'
-    Gdx.gl.gl_clear_color(r, g, b, 1)
-    Gdx.gl.gl_clear(GL20::GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT)
+    Gdx.gl.gl_viewport 0, 0, Gdx.graphics.width, Gdx.graphics.height
+    Gdx.gl.gl_clear_color(*ColorBuilder.rgb_from_hex('032D1D'), 1)
+    Gdx.gl.gl_clear GL20::GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT
 
     model_batch.begin camera
     model_batch.render model_instance, environment
@@ -66,9 +66,9 @@ class Game
   attr_reader :fps_logger, :camera, :model, :model_builder, :model_batch, :model_instance, :environment
 
   def init_camera
-    PerspectiveCamera.new(70, Gdx.graphics.width(), Gdx.graphics.height()).tap do |camera|
-      camera.position.set(10.0, 10.0, 10.0)
-      camera.look_at(0, 0, 0)
+    PerspectiveCamera.new(70, Gdx.graphics.width, Gdx.graphics.height).tap do |camera|
+      camera.position.set 10.0, 10.0, 10.0
+      camera.look_at 0, 0, 0
       camera.near = 1.0
       camera.far = 300.0
       camera.update
